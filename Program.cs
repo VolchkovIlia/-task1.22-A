0cfd8af50bc24d07262d29cc48172cc2539b3683
User <User@DESKTOP-9QVKBMG>
2022-09-19 23:19:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class Mainclass
+{
+public static void Main()
+    {
+        int x, y;
+        Console.WriteLine("Введите Х:");
+        x = int.Parse(Console.ReadLine());
+        y = 7 * x * x - 3 * x + 6;
+        Console.WriteLine("Значение функции {0}", y);
+    }
+}
